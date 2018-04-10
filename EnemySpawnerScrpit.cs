a090380a89cb3edbc04a974316d4b5acd8c253d0
GrizzlyGameDev <35743722+GrizzlyGameDev@users.noreply.github.com>
2018-04-10 14:00:25 -0400

    Add files via upload

---
+++ EnemySpawnerScrpit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScrpit : MonoBehaviour {
+
+    public GameObject Enemy;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 5f;
+    float nextSpawn = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(3.7f, 8.9f);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(Enemy, whereToSpawn, Quaternion.identity);
+        }
+	}
+}
