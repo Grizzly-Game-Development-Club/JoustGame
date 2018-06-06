06bd1a2710f9e021ff379cd5b8b3900ecba7ebe1
Reuben Massaquoi <Reubmann@gmail.com>
2018-06-05 21:13:10 -0400

    Implemented alternative approach to character script and input management

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+	
+	public int speed = 0;
+	private bool isFacingRight = false;
+	public float jumpVelocity = 0f;
+	public bool isGrounded = false;
+	Rigidbody2D characterRB;
+
+	//State variable
+	public bool isAttacking = false;
+	bool isJumping = false;
+	bool isWalking = false;
+
+	public int attackCounter = 0;
+
+	//Use for Transform Player Position if Touching Edge
+	float leftEdge;
+	float rightEdge;
+
+	float dirX = 0;
+	Vector2 vel;
+	//Use to check if player is dead
+	private bool isAlive;
+
+	//Reference to Game Manager Script
+	JoustGameManager gameManager;
+
+	// Use this for initialization
+	public void Start() {
+		characterRB = gameObject.GetComponent<Rigidbody2D>();
+		isAlive =true;
+		vel = new Vector2 (0, 0);
+		//Set Left and Right Border X Position
+		leftEdge = GameObject.Find("Edge Collider/Left").transform.position.x;
+		rightEdge = GameObject.Find("Edge Collider/Right").transform.position.x;
+		//Set Game Manager
+		gameManager = GameObject.Find("Game Manager").GetComponent<JoustGameManager>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+	public void Move() {
+		//set temp velocity value to RB velocity
+		vel = characterRB.velocity;
+
+		//Player Directions
+		if (dirX < 0.0f && !isFacingRight) {
+			FlipPlayer();
+		}
+		else if (dirX > 0.0f && isFacingRight) {
+			FlipPlayer();
+		}
+		//Character Moves
+		if (isWalking) {
+			vel.x = dirX * speed;
+		} 
+		if (isJumping)  
+		{
+			vel.y = jumpVelocity;
+		}
+		if (isAttacking)
+		{
+			if (isFacingRight)
+				dirX = -1;
+			else
+				dirX = 1;
+			Vector2 force = new Vector2(dirX * 500, 0);
+			characterRB.AddForce(force);
+			attackCounter--;
+			if (attackCounter <= 0)
+			{
+				attackCounter = 20;
+				isAttacking = false;
+				dirX = 0;
+			}
+		}
+		isJumping = false;
+		isWalking = false;
+		//print ("in here");
+		characterRB.velocity = vel;
+	}
+	public void Jump()
+	{
+		isJumping = true;
+	}
+
+	public void Attack()
+	{
+		if (!isAttacking)
+		{
+			isAttacking = true;                        
+		}
+
+	}
+	public void Walk(float dir)
+	{
+		dirX = dir;
+		isWalking = true;
+	}
+	void FlipPlayer() 
+	{
+		isFacingRight = !isFacingRight;
+		Vector2 localScale = gameObject.transform.localScale;
+		print (localScale.x);
+		localScale.x *= -1;
+		transform.localScale = localScale;
+	}
+	void OnCollisionEnter2D(Collision2D theCollision)
+	{
+
+		//Edge Reset
+		if (theCollision.gameObject.tag == "Edge")
+			switch (theCollision.gameObject.name) {
+		case "Left":                
+			this.transform.position = new Vector3(rightEdge - 0.7f, this.transform.position.y, this.transform.position.z);
+			break;
+		case "Right":      
+			this.transform.position = new Vector3(leftEdge + 0.7f, this.transform.position.y, this.transform.position.z);
+			break;
+		case "Bottom":
+			End();
+			break;
+		}
+
+		if (theCollision.gameObject.tag == "Ground")
+		{
+			isGrounded = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D theCollision)
+	{
+		if (theCollision.gameObject.tag == "Ground")
+		{
+			isGrounded = false;
+		}
+	}
+
+
+	//On Player Death
+	void End() {
+		Destroy(this);
+		//En.notify("CharacterDeath|id="+ID);
+		gameManager.setPlayerDeath(true);
+		gameManager.changeGameStatus("gameOverStatus", true);
+	}
+
+}
---
+++ Assets/Script/CharacterController_V1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController_V1 : Character {
+
+	// Use this for initialization
+	void Start () {
+		base.speed = 8;
+		base.jumpVelocity = 4f;
+		base.Start ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		base.Walk (Input.GetAxis("Horizontal"));
+		//print(Input.GetAxis("Horizontal"));
+
+		if (Input.GetKeyDown (KeyCode.X)) 
+		{
+			base.Attack ();
+		}
+		if (Input.GetButtonDown ("Jump")) 
+		{
+			base.Jump ();
+		}
+		base.Move ();
+	}
+}
