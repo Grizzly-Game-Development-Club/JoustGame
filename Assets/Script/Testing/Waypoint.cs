781e1dc010d062d87ae2351ef8da263f304c8cc7
Hieu Dinh <hiiamaalien@gmail.com>
2018-07-30 07:02:07 -0400

    Part 1

---
+++ Assets/Script/Testing/Waypoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+    public int WaypointID;
+    public WaypointType wayPointType;
+    public validWaypoint[] validWaypointConnection;
+
+    public enum Direction {LEFT,RIGHT};
+    public enum WaypointType { NORMAL, EDGE };
+
+    [System.Serializable]
+    public class validWaypoint
+    {
+        public GameObject waypointObject;
+        public Direction direction;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
