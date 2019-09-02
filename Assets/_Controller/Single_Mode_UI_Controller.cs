11fceed5060980e8879c8b21b00443a1d907bacb
Hieu Dinh <hiiamaalien@gmail.com>
2019-09-01 23:00:42 -0400

    Added EventManager

--- Assets/_Controller/Single_Mode_UI_Controller.cs
+++ Assets/_Controller/Single_Mode_UI_Controller.cs
@@ -1,9 +1,127 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Single_Mode_UI_Controller : MonoBehaviour
 {
+
+    #region Variable
+    [SerializeField] private GameObject m_UIPanel;
+    [SerializeField] private TextMeshProUGUI m_ScoreText;
+    [SerializeField] private TextMeshProUGUI m_TimeDurationText;
+    [SerializeField] private TextMeshProUGUI m_WaveText;
+
+
+    private int[] m_TimeLeft;
+    private int[] m_Wave;
+    #endregion
+
+    #region Getter & Setter
+    public GameObject UIPanel
+    {
+        get
+        {
+            return m_UIPanel;
+        }
+
+        set
+        {
+            m_UIPanel = value;
+        }
+    }
+    public TextMeshProUGUI ScoreText
+    {
+        get
+        {
+            return m_ScoreText;
+        }
+
+        set
+        {
+            m_ScoreText = value;
+        }
+    }
+    public TextMeshProUGUI TimeDurationText
+    {
+        get
+        {
+            return m_TimeDurationText;
+        }
+
+        set
+        {
+            m_TimeDurationText = value;
+        }
+    }
+    public TextMeshProUGUI WaveText
+    {
+        get
+        {
+            return m_WaveText;
+        }
+
+        set
+        {
+            m_WaveText = value;
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        m_TimeLeft = new int[2];
+        m_Wave = new int[2];
+    }
+
+    public void OnEnable()
+    {
+        EventManager.StartListening(E_EventName.Set_Level, SetLevel);
+        EventManager.StartListening(E_EventName.Set_Level, StartCountdown);
+        EventManager.StartListening(E_EventName.Pause_Game, StopCountdown);
+    }
+
+    private void StopCountdown(EventParam obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StartCountdown(EventParam obj)
+    {
+        Start
+    }
+
+    //When the Level is loaded, set up the wave num, and duration
+    public void SetLevel(EventParam obj)
+    {
+        E_EventName en = obj.EventName;
+        EventObject eo = obj.EventObject;
+
+        foreach (String es in eo.TypeString)
+        {
+            string[] esArray = es.Split(':');
+
+            switch (esArray[0])
+            {
+                case "Time Left Minute":
+                    m_TimeLeft[0] = int.Parse(esArray[1]);
+                    break;
+                case "Time Left Second":
+                    m_TimeLeft[1] = int.Parse(esArray[1]);
+                    break;
+                case "Wave Current":
+                    m_Wave[0] = int.Parse(esArray[1]);
+                    break;
+                case "Wave Total":
+                    m_Wave[1] = int.Parse(esArray[1]);
+                    break;
+            }
+        }
+
+        EventManager.FinishEvent(en);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +131,40 @@ public class Single_Mode_UI_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        TimeDurationText.SetText(String.Format("Time: {0}:{1}", m_TimeLeft[0], m_TimeLeft[1]));
+        WaveText.SetText(String.Format("Wave: {0}/{1}", m_Wave[0], m_Wave[1]));   
     }
+
+    void StartCountdown()
+    {
+
+
+    }
+
+    IEnumerator CountDownTime()
+    {
+        while (true)
+        {
+            int minute = m_TimeLeft[0];
+            int second = m_TimeLeft[1];
+
+            if (minute <= 0 && second <= 0)
+            {
+                EventManager.TriggerEvent(E_EventName.Game_Over);
+            }
+            if (second <= 0)
+            {
+                minute--;
+                second = 60;
+            }
+            
+            yield return new WaitForSeconds(1);
+            second--;
+
+            m_TimeLeft[0] = minute;
+            m_TimeLeft[1] = second;
+        }
+    }
+
 }
---
+++ Assets/_Model/EventManager.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_EventName
+{
+    Pause_Game, Resume_Game, Game_Over,
+    Start_Single_Mode, Set_Level, Start_Wave
+};
+
+public class EventManager : MonoBehaviour
+{
+    #region Variable
+    private static EventManager m_EventManager;
+    private static EventObject m_EventObject;
+
+    private Dictionary<E_EventName, Action<EventParam>> m_EventDictionary;
+    private bool m_EventDebugMode;
+    #endregion
+
+    #region Getter & Setter
+    public static EventManager Instance
+    {
+        get
+        {
+            if (!m_EventManager)
+            {
+                m_EventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+
+                if (!m_EventManager)
+                {
+                    Debug.LogError("There needs to be one active EventManger script on a GameObject in your scene.");
+                }
+                else
+                {
+                    m_EventManager.Init();
+                }
+            }
+
+            return m_EventManager;
+        }
+        set
+        {
+            m_EventManager = value;
+        }
+    }
+    public static EventObject EventObject
+    {
+        get
+        {
+            return m_EventObject;
+        }
+
+        set
+        {
+            m_EventObject = value;
+        }
+    }
+
+    public Dictionary<E_EventName, Action<EventParam>> EventDictionary
+    {
+        get
+        {
+            return m_EventDictionary;
+        }
+
+        set
+        {
+            m_EventDictionary = value;
+        }
+    }
+    public bool EventDebugMode
+    {
+        get
+        {
+            return m_EventDebugMode;
+        }
+
+        set
+        {
+            m_EventDebugMode = value;
+        }
+    }
+    #endregion
+
+    void Awake()
+    {
+        Instance = Instance;
+    }
+
+    void Init()
+    {
+        if (m_EventDictionary == null)
+        {
+            m_EventDictionary = new Dictionary<E_EventName, Action<EventParam>>();
+        }
+        if (m_EventObject == null)
+        {
+            m_EventObject = new EventObject();
+        }
+    }
+
+    public static void StartListening(E_EventName eventName, Action<EventParam> listener)
+    {
+        Action<EventParam> thisEvent;
+        if (Instance.EventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent += listener;
+            Instance.EventDictionary[eventName] = thisEvent;
+            EventDebugLog(String.Format("Event Name Registered: {0} \n Event Method Registered: {1}", eventName, listener.Method.Name));
+        }
+        else
+        {
+            thisEvent += listener;
+            Instance.m_EventDictionary.Add(eventName, thisEvent);
+            EventDebugLog(String.Format("Event Name Registered: {0} \n Event Method Registered: {1}", eventName, listener.Method.Name));
+        }
+
+    }
+
+    public static void StopListening(E_EventName eventName, Action<EventParam> listener)
+    {
+        if (m_EventManager == null) return;
+        Action<EventParam> thisEvent;
+        if (Instance.EventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent -= listener;
+            Instance.EventDictionary[eventName] = thisEvent;
+            EventDebugLog(String.Format("Event Name Unregistered: {0} \n Event Method Unregistered: {1}", eventName, listener.Method.Name));
+        }
+    }
+
+    public static void TriggerEvent(E_EventName eventName)
+    {
+        Action<EventParam> thisEvent;
+
+        if (Instance.EventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            EventDebugLog(String.Format("Event Started: {0}", eventName.ToString()));
+            thisEvent.Invoke(new EventParam(eventName));
+            EventDebugLog(String.Format("Event Ended: {0}", eventName.ToString()));
+        }
+    }
+
+    public static void TriggerEvent(E_EventName eventName, EventObject eventObject)
+    {
+
+        Action<EventParam> thisEvent;
+        if (Instance.EventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            EventDebugLog(String.Format("Event Started: {0}", eventName));
+            thisEvent.Invoke(new EventParam(eventName, eventObject));
+            EventDebugLog(String.Format("Event Ended: {0}", eventName));
+        }
+    }
+
+    public static void FinishEvent(E_EventName eventName)
+    {
+        EventDebugLog(String.Format("Event Finished: {0}", eventName));
+    }
+
+    public static void EventDebugLog(String eventDebugMessage)
+    {
+        if (Instance.m_EventDebugMode)
+        {
+            Debug.Log(String.Format("Event Log: \n {0} \n", eventDebugMessage));
+        }
+    }
+
+}
+
+public class EventObject
+{
+
+    #region Variable
+    [SerializeField] private List<string> m_TypeString;
+    [SerializeField] private List<bool> m_TypeBool;
+    [SerializeField] private List<int> m_TypeInt;
+    [SerializeField] private List<float> m_TypeFloat;
+    [SerializeField] private List<Transform> m_TypeTransform;
+    [SerializeField] private List<GameObject> m_TypeGameObject;
+    #endregion
+
+    #region Getter & Setter
+    public List<string> TypeString
+    {
+        get
+        {
+            return m_TypeString;
+        }
+
+        set
+        {
+            m_TypeString = value;
+        }
+    }
+    public List<bool> TypeBool
+    {
+        get
+        {
+            return m_TypeBool;
+        }
+
+        set
+        {
+            m_TypeBool = value;
+        }
+    }
+    public List<int> TypeInt
+    {
+        get
+        {
+            return m_TypeInt;
+        }
+
+        set
+        {
+            m_TypeInt = value;
+        }
+    }
+    public List<float> TypeFloat
+    {
+        get
+        {
+            return m_TypeFloat;
+        }
+
+        set
+        {
+            m_TypeFloat = value;
+        }
+    }
+    public List<Transform> TypeTransform
+    {
+        get
+        {
+            return m_TypeTransform;
+        }
+
+        set
+        {
+            m_TypeTransform = value;
+        }
+    }
+    public List<GameObject> TypeGameObject
+    {
+        get
+        {
+            return m_TypeGameObject;
+        }
+
+        set
+        {
+            m_TypeGameObject = value;
+        }
+    }
+    #endregion
+}
+
+
+[Serializable]
+public class EventParam
+{
+    #region Variable
+    private E_EventName m_EventName;
+    private EventObject m_EventObject;
+    #endregion
+
+    #region Constructor
+    public EventParam(E_EventName m_EventName)
+    {
+        this.m_EventName = m_EventName;
+        this.EventObject = new EventObject();
+    }
+
+    public EventParam(E_EventName m_EventName, EventObject m_EventObject)
+    {
+        this.m_EventName = m_EventName;
+        this.m_EventObject = m_EventObject;
+    }
+    #endregion
+
+    #region Getter & Setter
+    public E_EventName EventName
+    {
+        get
+        {
+            return m_EventName;
+        }
+
+        set
+        {
+            m_EventName = value;
+        }
+    }
+    public EventObject EventObject
+    {
+        get
+        {
+            return m_EventObject;
+        }
+
+        set
+        {
+            m_EventObject = value;
+        }
+    }
+    #endregion
+}
